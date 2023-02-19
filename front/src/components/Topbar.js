631ebb9984803b668a594e8616c78e9d273dbad7
doubaojiajia <93402304+doubaojiajia@users.noreply.github.com>
2023-02-19 12:55:02 -0500

    add files

---
+++ front/src/components/Topbar.js
@@ -0,0 +1,13 @@
+import "../static/css/main.css";
+import "../static/css/css.css";
+import "../static/css/css1.css";
+import "uikit/dist/js/uikit.js";
+
+//for input css
+export default function Topbar() {
+  return (
+    <div>
+      test
+    </div>
+  );
+}
