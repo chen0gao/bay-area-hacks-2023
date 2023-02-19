dc9e1d27299a37b4a7c540b1018eefdaeb3819b8
eh668 <74332706+eh668@users.noreply.github.com>
2023-02-19 15:37:08 -0500

    Added scroll bar to trip

--- front/src/components/TripHistory.jsx
+++ front/src/components/TripHistory.jsx
@@ -1,8 +1,18 @@
 import { Box, Container } from "@material-ui/core";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 
 function TripHistory({ data }) {
   return (
-    <Container>
+    <Container style ={{        width: '100%',
+    maxWidth: 360,
+    bgcolor: 'background.paper',
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: '90vh',
+    '& ul': { padding: 0 },}}>
       {data.map((ele) =>
         <Box
           key={ele.tripName}
@@ -10,12 +20,18 @@ function TripHistory({ data }) {
           m={2}
           style={{ background: 'white', borderRadius: '16px', border: '2px solid #000' }}
         >
-         <h2>Trip Name: {ele.tripName}</h2> 
-         <p>Date Traveled: {ele.tripDate}</p>
+        <li>
+            Name: {ele.name}
+            Lat: {ele.lat}
+            Lon: {ele.lng}
+        </li>
+
+        {/* <h2>Trip Name: {ele.tripName}</h2> 
+        <p>Date Traveled: {ele.tripDate}</p> */}
         </Box>
       )}
     </Container>
-  );
+    );
 }
 
 export default TripHistory;
