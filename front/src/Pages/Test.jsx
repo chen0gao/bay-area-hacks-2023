811a5b52ef42ae532240ec7657cae0aa0c79d6c3
jiajia <sijiay@seas.upenn.edu>
2023-02-19 15:02:30 -0500

    update login

--- front/src/Pages/Test.jsx
+++ front/src/Pages/Test.jsx
@@ -1,14 +1,22 @@
 import Topbar from "../components/Topbar";
 import {useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   
   const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  
 
   useEffect(() => {
-    if (!localStorage.getItem("user_info")) {
-      navigate("/login");
+    const fetchUser = async ()=>{
+      const res = localStorage.getItem("user_info");
+      const user = await res;
+      setUser(user);
+    }
+    fetchUser();
+    if(!user){
+      navigate("/");
     }
   }, []);
 
